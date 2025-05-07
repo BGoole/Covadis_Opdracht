5bf9e74760a6f2f3cce532ee2f114fd634642f2e
BGoole <bian.goole@student.graafschapcollege.nl>
2025-05-07 09:42:43 +0200

    Git init visual studio

--- CovadisLeenAuto.Domain/obj/Debug/net8.0/CovadisLeenAuto.Domain.AssemblyInfo.cs
+++ CovadisLeenAuto.Domain/obj/Debug/net8.0/CovadisLeenAuto.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CovadisLeenAuto.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff9f41ce2ae519156f9f65acb02264009943c53c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CovadisLeenAuto.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CovadisLeenAuto.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
