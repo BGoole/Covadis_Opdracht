96c2c3473fcf41310bb98fd379a0e8e093fa9fd7
BGoole <bian.goole@student.graafschapcollege.nl>
2025-06-11 10:55:33 +0200

    StoreRit functie aangemaakt. Services, repositories en interfaces aangemaakt

--- CovadisLeenAuto.Application/Interfaces/IRitRepository.cs
+++ CovadisLeenAuto.Application/Interfaces/IRitRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CovadisLeenAuto.Shared.DTO.Ritten;
 
 namespace CovadisLeenAuto.Application.Interfaces
 {
     public interface IRitRepository
     {
+        Task<int> StoreRit(StoreRitten rit);
     }
 }
--- CovadisLeenAuto.Application/Services/RitService.cs
+++ CovadisLeenAuto.Application/Services/RitService.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CovadisLeenAuto.Application.Interfaces;
+using CovadisLeenAuto.Domain.Data;
+using CovadisLeenAuto.Shared.DTO.Ritten;
 
 namespace CovadisLeenAuto.Application.Services
 {
-    public class RitService
+    public class RitService : IRitService
     {
-    }
+        private readonly IRitRepository ritRepository;
+        public RitService(IRitRepository ritRepository)
+        {
+            this.ritRepository = ritRepository;
+        }
+        public async Task<int> StoreRit(StoreRitten rit)
+        {
+            return await ritRepository.StoreRit(rit);
+        }
+    }   
 }
